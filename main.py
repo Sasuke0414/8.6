58656d7a85d2974ec1745cef0f48ec60f14b2a59
Abdulloh <163301172+Sasuke0414@users.noreply.github.com>
2024-09-30 21:24:02 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# mevalar = {
+#     "olma": 3,
+#     "banan": 5,
+#     "nok": 2
+# }
+
+# nomlari = mevalar.keys()
+# mqdorlari =  mevalar.values()
+# print(mqdorlari)
+# print(nomlari)
+# -----------------------------------------------------------------------
+# lugat = {
+#     "Toshkent": 2,
+#     "Parij": 2.1,
+#     "New York": 5.1
+# }
+
+# olsh = lugat.pop("Toshkent")
+
+# print(olsh)
+# print(lugat)
+# -------------------------------------------------------------------------
+# olma = {
+#     "olma": 10,
+#     "uzum": 70
+# }
+
+# for olma in range(olma.values()):
+#     print("olma")
+
+
+
+    
